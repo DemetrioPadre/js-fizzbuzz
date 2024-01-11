7108d3d9f5ae42fd3a5bf60ca993fa2f54705b57
DemetrioPadre <demetriopadre@gmail.com>
2024-01-11 16:37:45 +0100

    add-100 cards

--- javascript/main.js
+++ javascript/main.js
@@ -2,12 +2,13 @@ const cardsRow = document.getElementById('cards-row');
 
 for (let i = 1; i <= 100; i++) {
     console.log(i);
-    cardsRow.innerHTML +=
+    cardsRow.innerHTML += `
         <div class="col-3">
             <div class="card" >
                 <h3>card ${i}</h3>
             </div>
-        </div>;
+        </div>`;
+
 
 
 }
